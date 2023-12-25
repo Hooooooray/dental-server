8e7f0d38d5f68e3babbbb205f7556858c04995f4
杨云彬 <hooray@Binbins-MacBook-Air.local>
2023-12-25 21:15:16 +0800

    EmployeeTable Operation

--- controller/employee.js
+++ controller/employee.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/employee/add', checkSchema({
     name: { notEmpty: true, errorMessage: '员工姓名不能为空' },
     position: { notEmpty: true, errorMessage: '员工岗位不能为空' },
-    department:{notEmpty:true,errorMessage:'部门不能为空'},
+    department: { notEmpty: true, errorMessage: '部门不能为空' },
     dentalDepartment: { notEmpty: true, errorMessage: '牙医科室不能为空' },
     gender: { notEmpty: true, errorMessage: '性别不能为空' },
 }), async (req, res) => {
@@ -89,6 +89,67 @@ router.get('/employee', checkSchema({
 router.get('/employees', async (req, res) => {
     try {
         const employees = await prisma.Employee.findMany();
+
+        // 定义一个枚举类型的映射表
+        const enumMap = {
+            EmployeePosition: {
+                ADMINISTRATOR: '管理员',
+                DOCTOR: '医生',
+                NURSE: '护士',
+                FRONT_DESK: '前台',
+                ADMINISTRATIVE: '行政',
+                FINANCE: '财务',
+                ASSISTANT: '助理',
+                COUNSELOR: '咨询师',
+                CUSTOMER_SERVICE: '客服',
+                PROCUREMENT: '采购',
+                WAREHOUSE_KEEPER: '库管',
+            },
+            Department: {
+                MEDICAL: '医疗部',
+                COUNSELING: '咨询部',
+                OPERATIONS: '经营部',
+                LOGISTICS: '后勤部',
+                FINANCE: '财务部',
+            },
+            DentalDepartment: {
+                GENERAL_DENTISTRY: '综合牙科',
+                DENTAL_IMPLANTATION: '牙医种植',
+                DENTAL_ORTHODONTICS: '牙医正畸',
+                DENTAL_RESTORATION: '牙医修复',
+                DENTAL_CARE: '牙医护理',
+                CHILDRENS_TEETH: '儿童牙科',
+                PERIODONTAL: '牙周科',
+                ORTHODONTICS: '矫正科',
+                TOOTH_EXTRACTION: '牙齿拔除',
+                TOOTH_INLAY: '牙齿镶嵌',
+            },
+            EmploymentStatus: {
+                ACTIVE: '在职',
+                ON_LEAVE: '休假',
+                PROBATION: '试用期',
+                RESIGNED: '离职',
+            },
+            Gender: {
+                MALE: '男',
+                FEMALE: '女',
+                UNKNOWN: '未知'
+            }
+        };
+
+        // 遍历员工列表，将枚举字段换成相应的中文
+        employees.forEach((employee) => {
+            // 岗位
+            employee.position = enumMap.EmployeePosition[employee.position];
+            // 部门
+            employee.department = enumMap.Department[employee.department];
+            // 牙医科室
+            employee.dentalDepartment = enumMap.DentalDepartment[employee.dentalDepartment];
+            // 在职状态
+            employee.employmentStatus = enumMap.EmploymentStatus[employee.employmentStatus];
+            // 性别
+            employee.gender = enumMap.Gender[employee.gender]
+        });
         res.success(employees);
     } catch (error) {
         console.error('Error fetching employees:', error);
@@ -96,8 +157,9 @@ router.get('/employees', async (req, res) => {
     }
 });
 
-router.get('/employee/maxID',async (req,res)=>{
-    try{
+// 查询员工号最大值
+router.get('/employee/maxID', async (req, res) => {
+    try {
         const id = await prisma.Employee.findFirst({
             select: {
                 employeeID: true,
@@ -105,7 +167,7 @@ router.get('/employee/maxID',async (req,res)=>{
             orderBy: { employeeID: 'desc' },
         });
         res.success(id);
-    }catch (error){
+    } catch (error) {
         console.error('Error fetching employees:', error);
         res.fail('查询员工失败');
     }
